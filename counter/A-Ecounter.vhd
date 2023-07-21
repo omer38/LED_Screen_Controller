027679b8e8660e06863c7578239a9fb5e69cde15
Ömer Tuğrul <omertugrul24@gmail.com>
2023-07-21 17:11:41 +0300

    Update A-Ecounter.vhd

--- counter/A-Ecounter.vhd
+++ counter/A-Ecounter.vhd
@@ -13,7 +13,7 @@ end UP_COUNTER;
 architecture Behavioral of UP_COUNTER is
 
 signal counter_up: std_logic_vector(4 downto 0);
-signal counter_clk : std_logic_vector(1 downto 0) := "11" ;-- to get 4
+signal counter_clk : integer range 0 to 1520 := 1519;
 
 begin
 -- up counter
@@ -22,13 +22,13 @@ begin
 if(rising_edge(clk)) then
     if(reset='1') then
          counter_up <= "00000";
-    else
-        if counter_clk = "00" then  
-            counter_up <= counter_up + "00001";
-            counter_clk <= counter_clk - "01";
-        else
-            counter_clk <= counter_clk - "01";
-        end if;
+    else 
+       if counter_clk = 0 then
+          counter_up <= counter_up + "00001";
+          counter_clk <= 1519;
+       else
+          counter_clk <= counter_clk - 1;
+    end if;
     end if;
  end if;
 end process;
