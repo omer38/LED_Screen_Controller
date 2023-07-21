f0ce6a4553a277ca62d452d5496dff51cfab06e8
Ömer Tuğrul <omertugrul24@gmail.com>
2023-07-21 17:10:21 +0300

    Update hoe1.vhd

--- hoe1.vhd
+++ hoe1.vhd
@@ -3,20 +3,21 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
-entity hoe1_sim is
+entity hoe1 is
     Port (
         clk : in STD_LOGIC;
-        pulse_out : out STD_LOGIC := '0'
+        pulse_out : out STD_LOGIC := '1'
     );
-end entity hoe1_sim;
+end entity hoe1;
 
-architecture Behavioral of hoe1_sim is
-    constant ON_TIME : integer := 300;   -- 3 microseconds in 10 ns (basys period) units to count clock pulses 
-    constant OFF_TIME : integer := 49700; -- 497 microseconds in 10 ns (basys period) units to count clock pulses
-    signal counter_on : integer range 0 to ON_TIME - 1 := 299;
-    signal counter_off : integer range 0 to OFF_TIME -1 := 49699;
+architecture Behavioral of hoe1 is
+    constant ON_TIME : integer := 16;   -- 3 microseconds in 10 ns (basys period) units to count clock pulses 
+    constant OFF_TIME : integer := 1520; -- 497 microseconds in 10 ns (basys period) units to count clock pulses
+    signal counter_on : integer range 0 to ON_TIME - 1 := 15;
+    signal counter_off : integer range 0 to OFF_TIME -1 := 1519;
     type t_state is (S_ON,S_OFF);
     signal state : t_state := S_ON;
+    signal pulse_mid : std_logic := '1';
 begin
     process(clk)
     begin
@@ -29,7 +30,7 @@ begin
                        counter_off <= OFF_TIME - 1; 
                     else
                         counter_on <= counter_on - 1;
-                        pulse_out <= '1' ;
+                        pulse_mid <= '1' ;
                     end if;
                     
                 when S_OFF => 
@@ -37,10 +38,11 @@ begin
                         state <= S_ON;
                     else
                         counter_off <= counter_off -1 ;
-                        pulse_out <= '0';
+                        pulse_mid <= '0';
                     end if; 
                 
              end case;
          end if;                  
     end process;
+    pulse_out <= pulse_mid;
 end Behavioral;
