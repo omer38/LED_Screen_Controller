056b44fc7132ce38b90506793c4a0408e1cf90c8
Ömer Tuğrul <omertugrul24@gmail.com>
2023-07-31 11:16:30 +0300

    Update clk_12_5_mhz.vhd

--- HDCLK/clk_12_5_mhz.vhd
+++ HDCLK/clk_12_5_mhz.vhd
@@ -9,7 +9,7 @@ entity clk_12_5_mhz is
 end clk_12_5_mhz;
 
 architecture Behavioral of clk_12_5_mhz is
-    constant DIVIDER : integer := 2; -- Divide 100 MHz input clock by 25 to get 4 MHz output
+    constant DIVIDER : integer := 4; -- Divide 100 MHz input clock by 25 to get 4 MHz output
     signal counter : integer range 0 to DIVIDER - 1 := 0;
     signal clk_div : STD_LOGIC := '0';
     
